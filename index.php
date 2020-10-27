629f16dc2b577f181f36d8f14c66fcba8f447f08
tatsuki-bb <73352046+tatsuki-bb@users.noreply.github.com>
2020-10-27 22:37:43 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>マッチング　ログインページ</title>
+</head>
+<body>
+    <h1>マッチング　ログインページ</h1>
+    <h2>登録済みのメールアドレスとパスワードを入力しログインしてください</h2>
+    <form action="" method="post" enctype="multipart/form-data">
+    <dl>
+        <dt>メールアドレス</dt>
+        <dd><input type="text" name="email" size="35" maxlength="255"></dd>
+        <dt>パスワード</dt>
+        <dd><input type="text" name="pasward" size="35" maxlength="255"></dd>
+    </dl>
+    <input type="submit" value="ログインする">
+    <a href="">会員登録がまだの方はこちら</a>
+    </form>
+</body>
+</html>
